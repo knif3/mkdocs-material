1897fb9fa0fdd3b0a232b382c2fe89df758d40d3
Roland Szuts <knif3@envio.hu>
2024-12-16 10:55:25 +0100

    chore: change workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -eu
-cd "$GITHUB_WORKSPACE/$INPUT_WORKDIR"
+cd "$GITHUB_WORKSPACE"
+cd "$INPUT_WORKDIR"
 
 sh -c "mkdocs build -c --config-file $INPUT_CONFIG_FILE --site-dir $INPUT_OUTPUT_DIR"
