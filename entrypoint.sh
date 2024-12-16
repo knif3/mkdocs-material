da35ab96f19a46a7fd9286ebe0bb1dba4d367f7a
Roland Szuts <knif3@envio.hu>
2024-12-16 11:29:48 +0100

    fix: build path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@ mv "$INPUT_CONFIG_FILE" ../
 cd ..
 
 sh -c "mkdocs build -c --config-file $INPUT_CONFIG_FILE --site-dir $GITHUB_WORKSPACE/../$INPUT_OUTPUT_DIR"
+mv "$GITHUB_WORKSPACE/../$INPUT_OUTPUT_DIR" "$GITHUB_WORKSPACE/$INPUT_OUTPUT_DIR"
