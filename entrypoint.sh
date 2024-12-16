8be131974195e76cffbe3f82da530e09bc879ef4
Roland Szuts <knif3@envio.hu>
2024-12-16 11:15:55 +0100

    fix: paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -eu
+
+mv "$INPUT_CONFIG_FILE" ../
+cd ..
 cd "$GITHUB_WORKSPACE/$INPUT_WORKDIR"
 
-sh -c "mkdocs build -c --config-file $GITHUB_WORKSPACE/$INPUT_CONFIG_FILE --site-dir $GITHUB_WORKSPACE/$INPUT_OUTPUT_DIR"
+sh -c "mkdocs build -c --config-file $INPUT_CONFIG_FILE --site-dir $INPUT_OUTPUT_DIR"
