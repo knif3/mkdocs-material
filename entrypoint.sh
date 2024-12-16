eb1009805238a4ff14364a115328395a77a34ed1
Roland Szuts <knif3@envio.hu>
2024-12-16 11:03:58 +0100

    fix: command line

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -eu
 cd "$GITHUB_WORKSPACE/$INPUT_WORKDIR"
 
-sh -c "mkdocs build -c --config-file $GITHUB_WORKSPACE/$INPUT_CONFIG_FILE --site-dir $GITHUB_WORKSPACE/$INPUT_OUTPUT_DIR ."
+sh -c "mkdocs build -c --config-file $GITHUB_WORKSPACE/$INPUT_CONFIG_FILE --site-dir $GITHUB_WORKSPACE/$INPUT_OUTPUT_DIR"
